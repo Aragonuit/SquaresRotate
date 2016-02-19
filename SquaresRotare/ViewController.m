289de118db754d05fe2e8201587e1a73c8a21479
AragonUit <aragonuit@MacBook-Air.local>
2016-02-19 15:51:47 +0700

    first commit

--- SquaresRotare/ViewController.m
+++ SquaresRotare/ViewController.m
@@ -15,13 +15,49 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self drawNestedSquares];
+    [self performSelector:@selector(rotateAllSquares) withObject:nil afterDelay:2.0];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)drawNestedSquares{
+    CGSize mainViewSize = self.view.bounds.size;
+    CGFloat margin = 20 ;
+    CGFloat recWith = mainViewSize.width - margin *2.0;
+    CGFloat statusNavigationBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height ;
+    CGPoint center = CGPointMake(mainViewSize.width * 0.5, (mainViewSize.height+statusNavigationBarHeight)*0.5);
+    for (int i=0; i<=10; i++) {
+        UIView* square;
+        if (i%2==0) {
+          square =[self drawSquaresByWith:recWith andRotate:NO atCenter:center];
+        }else
+        {
+            square=[self drawSquaresByWith:recWith andRotate:YES atCenter:center];
+        }
+        [self.view addSubview:square];
+        recWith=recWith*0.707;
+    }
 }
-
+-(UIView*)drawSquaresByWith:(CGFloat)with
+               andRotate:(BOOL)rotate
+                atCenter:(CGPoint)center{
+    UIView*square = [[UIView alloc]initWithFrame:CGRectMake(0, 0, with, with)];
+    square.center = center;
+   
+    square.backgroundColor=rotate ? [UIColor whiteColor] : [UIColor darkGrayColor];
+    square.transform=rotate ? CGAffineTransformMakeRotation(M_PI_4) : CGAffineTransformIdentity;
+    
+     return square;
+}
+-(void)rotateAllSquares{
+    [UIView animateWithDuration:2.0 animations:^{
+        for (int i=0; i<self.view.subviews.count;i++) {
+            if (i%2==0) {
+                ((UIView*)self.view.subviews[i]).transform=CGAffineTransformMakeRotation(M_PI_4);
+            }else
+            {
+                ((UIView*)self.view.subviews[i]).transform=CGAffineTransformIdentity;
+            }
+        }
+    }];
+}
 @end
